29c2f2988a4fafb1bfb39a56e586a9f01f7a4f6c
tiara <tiara@tiara>
2018-12-03 13:43:51 +0700

    commit biodata

--- src/main/java/com/miniproject/training/controller/BiodataController.java
+++ src/main/java/com/miniproject/training/controller/BiodataController.java
@@ -3,12 +3,18 @@ package com.miniproject.training.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.miniproject.training.model.Biodata;
 import com.miniproject.training.service.BiodataService;
@@ -29,9 +35,23 @@ public class BiodataController {
 	
 	@RequestMapping(value="/save", method=RequestMethod.POST)
 	@ResponseBody
-	public Biodata save(@RequestBody Biodata biodata) {
+	public Biodata save(@RequestBody Biodata biodata, RedirectAttributes redirectAttributes) {
 		biodataService.save(biodata);
 		return biodata;
 	}
 	
+	@RequestMapping(value="/editui/{id}", method=RequestMethod.GET)
+	@ResponseBody
+	public Biodata editUI(@PathVariable("id") Long id) {
+		Biodata biodata=biodataService.getBiodataById(id);
+		return biodata;
+	}
+	
+	@RequestMapping(value="/editbiodata", method=RequestMethod.POST)
+	@ResponseBody
+	public Biodata editBiodata(@RequestBody Biodata biodata) {
+		biodataService.save(biodata);
+		return biodata;
+	}
+
 }
