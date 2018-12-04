fa492e25b3875476966dc650f5b31fa256e12b01
masudbayu <masudbayu@DESKTOP-SGU9DF7>
2018-12-05 01:41:47 +0700

    commit nambah batch sama class

---
+++ src/main/java/com/miniproject/training/model/Batch.java
@@ -0,0 +1,10 @@
+package com.miniproject.training.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_Batch")
+public class Batch {
+
+}
---
+++ src/main/java/com/miniproject/training/model/Class.java
@@ -0,0 +1,10 @@
+package com.miniproject.training.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_Class")
+public class Class {
+
+}
